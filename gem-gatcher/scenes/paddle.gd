f76e588355f04bf5d87bbeec3a015328eee817e5
Paulo <pmatos2000@gmail.com>
2024-08-28 18:57:35 -0300

    Signals

--- gem-gatcher/scenes/paddle.gd
+++ gem-gatcher/scenes/paddle.gd
@@ -7,3 +7,6 @@ func _process(delta: float) -> void:
 		position.x -= speed * delta
 	elif Input.is_action_pressed("right"):
 		position.x += speed * delta
+
+func _on_gem_area_entered(area: Area2D) -> void:
+	print(area)
