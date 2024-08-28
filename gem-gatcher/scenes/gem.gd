cf58f3a0ceaaf74e3721a1411836024d2ba9b6a5
Paulo <pmatos2000@gmail.com>
2024-08-28 17:55:11 -0300

    Removing the Gem

--- gem-gatcher/scenes/gem.gd
+++ gem-gatcher/scenes/gem.gd
@@ -4,3 +4,7 @@ const speed: float = 100.0
 
 func _process(delta: float) -> void:
 	position.y += speed * delta
+	
+	if position.y > get_viewport_rect().size.y:
+		set_process(false)
+		queue_free()
