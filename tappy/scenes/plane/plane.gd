a6c2f2878d22704612cd2ef7d96cfc27ebef20a0
Paulo <pmatos2000@gmail.com>
2024-08-29 17:29:49 -0300

    Collisions

--- tappy/scenes/plane/plane.gd
+++ tappy/scenes/plane/plane.gd
@@ -3,12 +3,22 @@ extends CharacterBody2D
 const GRAVITY : float = 600.0
 const POWER : float = -200.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
 func _physics_process(delta: float) -> void:
 	velocity.y += delta * GRAVITY
 	fly()
 	move_and_slide()
+	if is_on_floor():
+		die()
 
 
 func fly() -> void:
 	if Input.is_action_just_pressed("fly"):
 		velocity.y = POWER
+
+
+func die() -> void:
+	set_physics_process(false)
+	animated_sprite.stop()
