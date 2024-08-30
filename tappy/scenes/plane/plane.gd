e7c0110c3cb6f51857cf76791a92ce0a06f022d8
Paulo <pmatos2000@gmail.com>
2024-08-30 00:08:51 -0300

    AnimationPlayer  -  power

--- tappy/scenes/plane/plane.gd
+++ tappy/scenes/plane/plane.gd
@@ -4,6 +4,7 @@ const GRAVITY : float = 600.0
 const POWER : float = -200.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _physics_process(delta: float) -> void:
@@ -17,6 +18,7 @@ func _physics_process(delta: float) -> void:
 func fly() -> void:
 	if Input.is_action_just_pressed("fly"):
 		velocity.y = POWER
+		animation_player.play("power")
 
 
 func die() -> void:
