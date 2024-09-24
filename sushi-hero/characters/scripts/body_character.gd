8fe9fb63f6be71af38e412071cf4c040ebf89061
Paulo <pmatos2000@gmail.com>
2024-09-24 02:22:47 -0300

    Cirreção bug interpolação

--- sushi-hero/characters/scripts/body_character.gd
+++ sushi-hero/characters/scripts/body_character.gd
@@ -1,10 +1,10 @@
 extends Node3D
 class_name CharacterArmature
 
-const _LERP_VELOCITY: float = 0.15
+const _LERP_VELOCITY: float = 0.10
 
 func apply_rotation(velocity: Vector3) -> void:
-	rotation.y = lerp(
+	rotation.y = lerp_angle(
 		rotation.y,
 		atan(-velocity.x/-velocity.z),
 		_LERP_VELOCITY
