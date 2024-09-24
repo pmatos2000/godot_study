dbaca7ec37a645d48d98709e65aa1f38508a6e6a
Paulo <pmatos2000@gmail.com>
2024-09-24 17:41:02 -0300

    atan para atan2

--- sushi-hero/characters/scripts/body_character.gd
+++ sushi-hero/characters/scripts/body_character.gd
@@ -9,7 +9,7 @@ const _LERP_VELOCITY: float = 0.10
 func apply_rotation(velocity: Vector3) -> void:
 	rotation.y = lerp_angle(
 		rotation.y,
-		atan(-velocity.x/-velocity.z),
+		atan2(-velocity.x, -velocity.z),
 		_LERP_VELOCITY
 	)
 
