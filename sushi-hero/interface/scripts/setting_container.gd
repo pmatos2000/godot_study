c17f29c4ee6dabef3cc6d13ec78da1bbba4fa7ef
Paulo <pmatos2000@gmail.com>
2024-09-25 17:40:11 -0300

    Menu

--- sushi-hero/interface/scripts/setting_container.gd
+++ sushi-hero/interface/scripts/setting_container.gd
@@ -10,12 +10,15 @@ func _on_check_box_full_screen(toggled_on: bool) -> void:
 
 
 func _on_option_button_vsync_item_selected(index: int) -> void:
-	if index == 0:
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-	elif index == 1:
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
-	elif index == 2:
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ADAPTIVE)
+	match  index:
+		0:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		1:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+		2:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ADAPTIVE)
+		_:
+			DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 
 
 func _on_check_box_display_fps(toggled_on: bool) -> void:
@@ -23,3 +26,7 @@ func _on_check_box_display_fps(toggled_on: bool) -> void:
 		var ui_class: UiClass = Ui as UiClass;
 		if ui_class.fps_container:
 			ui_class.fps_container.visible = toggled_on;
+
+
+func _on_option_button_screen_space_item_selected(index: int) -> void:
+	pass
