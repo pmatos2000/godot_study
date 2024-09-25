430ecca89d7a4a40c99afe70ad182177179be92a
Paulo <pmatos2000@gmail.com>
2024-09-25 20:13:50 -0300

    Configuração

--- sushi-hero/interface/scripts/setting_container.gd
+++ sushi-hero/interface/scripts/setting_container.gd
@@ -4,7 +4,7 @@ class_name SettingContainer
 
 func _on_check_box_full_screen(toggled_on: bool) -> void:
 	if toggled_on:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
@@ -29,4 +29,28 @@ func _on_check_box_display_fps(toggled_on: bool) -> void:
 
 
 func _on_option_button_screen_space_item_selected(index: int) -> void:
-	pass
+	get_viewport().screen_space_aa = index
+
+
+func _on_option_button_msaa3d_item_selected(index: int) -> void:
+	get_viewport().msaa_3d = index
+
+
+func _on_check_box_taa(toggled_on: bool) -> void:
+	get_viewport().use_taa = toggled_on
+
+
+func _on_check_box_reflections(toggled_on: bool) -> void:
+	GlobalWorldEnvironment.environment.ssr_enabled = toggled_on
+
+
+func _on_check_box_ambient_occlusion(toggled_on: bool) -> void:
+	GlobalWorldEnvironment.environment.ssao_enabled = toggled_on
+
+
+func _on_check_box_indirect_lighting(toggled_on: bool) -> void:
+	GlobalWorldEnvironment.environment.ssil_enabled = toggled_on
+
+
+func _on_check_box_global_illumination(toggled_on: bool) -> void:
+	GlobalWorldEnvironment.environment.sdfgi_enabled = toggled_on # Replace with function body.
